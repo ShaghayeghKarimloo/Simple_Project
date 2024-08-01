e0d179599ea54e11b15bad550e5a5db06990c7ad
shaghayegh <shaghayegh.karimloo@gmail.com>
2024-08-01 22:49:26 +0330

    This file JavaScript file for website

---
+++ p1.js
@@ -0,0 +1,5 @@
+document.querySelector('.registration-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // اضافه کردن کدهای لازم برای ثبت‌نام
+    alert('ثبت‌نام با موفقیت انجام شد!');
+});
